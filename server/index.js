d477e041cfbfadef0980e51e109538f25143a755
Nathaniel Olsen <nate.id@Nathaniels-MBP.lan>
2023-07-28 11:49:44 -0600

    First commit

---
+++ server/index.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import multer from 'multer';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+//Configurations
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(helmet);
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+// file storage
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function (req, filie, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage});
+
+//routes with files
+app.post("/auth/register", upload.single("picture"), register)
+
+//mongoose set up 
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+}).catch((error) => console.log(`${error} did not connect`));
